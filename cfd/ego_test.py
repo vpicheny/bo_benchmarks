63ce9c5b9cfff4647b586aef7590c1db70725d3b
Andrei Paleyes <apalees@gmail.com>
2021-11-19 13:23:28 +0000

    Parse args, read existing init data

--- cfd/ego_test.py
+++ cfd/ego_test.py
@@ -1,3 +1,19 @@
+######### read igloo bin path from stdin ###############
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("igloo_bin_path", type=str, help="Path to Igloo bin directory. Required.")
+parser.add_argument("--init-data-path", type=str, default=None, help="Path to optim.dat file that shall be used for initial data. If not given, new initial data is generated.")
+parser.add_argument("--n-init-points", type=int, default=None, help="Nummber of initial points to generate. Defaults to (2 * number of parameters). Ignored if path to initial data is given.")
+
+args = parser.parse_args()
+
+igloo_bin_path = args.igloo_bin_path
+init_data_path = args.init_data_path  # None if not given
+n_init_points = args.n_init_points  # None if not given
+
+
+######## imports, boilerplate code to work with Igloo ###########
 import numpy as np
 import subprocess
 
@@ -5,12 +21,10 @@ import time
 import timeit
 import os
 import pathlib
-import sys
 
 import tensorflow as tf
 import trieste
 import gpflow
-
 
 def try_get_number(s):
     try:
@@ -99,20 +113,7 @@ class IglooSimulationRunner():
         return objective_value, constraint_value
 
 
-
-######### read igloo bin path from stdin ###############
-# we can consider adding more complex options, like name of the run, later
-# in which case argsparse shall probably be used here
-# or other package for proper args parsing
-# for now this simple approach shall do
-
-if len(sys.argv) != 2:
-    print("Path to igloo bin is required")
-    sys.exit(1)
-
-igloo_bin_path = sys.argv[1]
 igloo_cfd = IglooSimulationRunner(igloo_bin_path)
-
 
 ######### Trieste observer and search space ###############
 
@@ -122,10 +123,13 @@ OBJECTIVE = "OBJECTIVE"
 CONSTRAINT = "CONSTRAINT"
 FAILURE = "FAILURE"
 
+
 def observer(query_points):
     objective_values = []
     constraint_values = []
     failure_values = []
+    ok_query_points = []
+
     for point in query_points:
         ov, cv = igloo_cfd(point)
         if ov is None or cv is None:
@@ -135,10 +139,11 @@ def observer(query_points):
             failure_values.append([1.0])
             objective_values.append([ov])
             constraint_values.append([cv])
+            ok_query_points.append(point)
 
     return {
-        OBJECTIVE: Dataset(query_points, tf.convert_to_tensor(objective_values, dtype=tf.float64)),
-        CONSTRAINT: Dataset(query_points, tf.convert_to_tensor(constraint_values, dtype=tf.float64)),
+        OBJECTIVE: Dataset(tf.convert_to_tensor(ok_query_points, dtype=tf.float64), tf.convert_to_tensor(objective_values, dtype=tf.float64)),
+        CONSTRAINT: Dataset(tf.convert_to_tensor(ok_query_points, dtype=tf.float64), tf.convert_to_tensor(constraint_values, dtype=tf.float64)),
         FAILURE: Dataset(query_points, tf.convert_to_tensor(failure_values, dtype=tf.float64))
     }
 
@@ -151,12 +156,76 @@ search_space = trieste.space.Box(
 
 ######### Collecting initial data ###############
 
-num_initial_points = 2 * search_space.dimension  # R code uses twice that
-# num_initial_points = 3
-initial_query_points = search_space.sample(num_initial_points)
+def read_run_data(optim_dat_path):
+    """Given a path to optim.dat file that contains data for a series of Igloo runs
+    returns a dictionary of Trieste datasets built from that data.
 
-start = timeit.default_timer()
-initial_data = observer(initial_query_points)
-stop = timeit.default_timer()
+    We assume that optim.dat has one or more lines with the following format:
+    ID objective constraint input1 ... input16
+    ID NaN input1 ... input16
 
-print(f"Time for initial points: {stop - start:.0f}s")
+    Above first line is for a successful Igloo run, second is for a failed run
+    """
+    if not os.path.isfile(optim_dat_path) or not optim_dat_path.endswith("optim.dat"):
+        raise ValueError(f"{optim_dat_path} is not a valid path to optim.dat")
+
+    with open(optim_dat_path) as f:
+        all_lines = f.readlines()
+
+    objective_values = []
+    constraint_values = []
+    failure_values = []
+    ok_query_points = []
+    all_query_points = []
+
+    for line in all_lines:
+        if len(line) == 0:
+            # skip empty lines
+            continue
+
+        tokens = line.split(" ")
+        tokens = tokens[1:] # skip first column with run ID
+        
+        query_point = [float(x) for x in tokens[-search_space.dimension:]]
+        all_query_points.append(query_point)
+        if tokens[0] == 'NaN':
+            # got failure line
+            # instead of values for objective and constraint
+            # failure lines contains single NaN
+            assert len(tokens) == search_space.dimension + 1
+            failure_values.append([0.0])
+        else:
+            # got good line
+            assert len(tokens) == search_space.dimension + 2
+            objective_value = float(tokens[0])
+            constraint_value = float(tokens[1])
+
+            failure_values.append([1.0])
+            objective_values.append([objective_value])
+            constraint_values.append([constraint_value])
+            ok_query_points.append(query_point)
+
+    return {
+        OBJECTIVE: Dataset(tf.convert_to_tensor(ok_query_points, dtype=tf.float64), tf.convert_to_tensor(objective_values, dtype=tf.float64)),
+        CONSTRAINT: Dataset(tf.convert_to_tensor(ok_query_points, dtype=tf.float64), tf.convert_to_tensor(constraint_values, dtype=tf.float64)),
+        FAILURE: Dataset(tf.convert_to_tensor(all_query_points, dtype=tf.float64), tf.convert_to_tensor(failure_values, dtype=tf.float64))
+    }
+
+if init_data_path is None:
+    # generate new initial data
+    num_initial_points = 2 * search_space.dimension if n_init_points is None else n_init_points
+    initial_query_points = search_space.sample(num_initial_points)
+
+    start = timeit.default_timer()
+    initial_data = observer(initial_query_points)
+    stop = timeit.default_timer()
+
+    print(f"Time for initial points: {stop - start:.0f}s")
+else:
+    # read existing initial data
+    initial_data = read_run_data(init_data_path)
+    num_initial_points = len(initial_data[FAILURE])  # failure dataset contains all points, unlike other two
+
+print("Initial data:")
+for tag in initial_data:
+    print(f"{tag}: {len(initial_data[tag])} points")
