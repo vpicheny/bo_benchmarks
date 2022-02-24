47a815893df87b3eaf1fab2334cdc7046f9164fe
henrymoss <henry.moss@secondmind.ai>
2022-02-24 08:08:00 +0000

    WIP

--- quantile/compare_results.py
+++ quantile/compare_results.py
@@ -61,3 +61,4 @@ for tag in pb_tags:
             all_regrets[exp_name] = regret.T
 
     fig = plot_regret(all_regrets, title=tag, ylabel="Simple regret", show_all=False, quantiles=True, CI=False)
+    fig.savefig(tag+"regret.png")
